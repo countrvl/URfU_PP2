28cf405bf3f8312058bbbd80ffe54d07d7863288
KovshefulCoder <84292117+KovshefulCoder@users.noreply.github.com>
2025-05-05 15:28:00 +0300

    fix /start command for users bot

--- telegram/users_bot/handlers/start.py
+++ telegram/users_bot/handlers/start.py
@@ -14,4 +14,4 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Привет, {html.bold(message.from_user.first_name + " " + message.from_user.last_name)}!")
+    await message.answer(f"Привет, {html.bold(" ".join(filter(None, [message.from_user.first_name, message.from_user.last_name])))}!")
