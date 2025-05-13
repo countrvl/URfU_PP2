78f3d17e954b5636c59e72424ce9f07cd493e59d
Elikul <linotska@inbox.ru>
2025-05-13 23:31:18 +0300

    feat: добавить пайплайн для ml модуля

---
+++ ml/fit.py
@@ -0,0 +1,18 @@
+import cv2
+import torch
+from ultralytics import YOLO  # Используем YOLOv8 для детекции объектов
+import easyocr  # Библиотека для распознавания текста
+import os
+import numpy as np
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="5pXFfA6Z3uRGVkNdeUMD")
+project = rf.workspace("roboflow-universe-projects").project("license-plate-recognition-rxg4e")
+version = project.version(4)
+dataset = version.download("yolov8")
+
+# Загрузка предобученной модели YOLOv8
+model = YOLO('yolov8n.pt')
+
+# Обучение модели
+results = model.train(data=f"{dataset.location}/data.yaml", epochs=10, imgsz=640, device='cuda')
